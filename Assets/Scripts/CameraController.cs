52255d3aebc6afe51d8e3deba0207dea917f0a45
JulianaHaurilava <jrediss.k@gmail.com>
2023-10-17 14:52:04 +0300

    player movement added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Vector3 offset;
+    [Header("References")]
+    [SerializeField] Transform orientation;
     [SerializeField] Transform player;
+    [SerializeField] Transform playerObj;
+    [SerializeField] Rigidbody rb;
 
-    void Update()
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Transform combatLookAt;
+
+    private void Update()
     {
-        transform.position = player.transform.position + offset;
+        // rotate orientation
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        // roate player object
+        Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+        orientation.forward = dirToCombatLookAt.normalized;
+
+        playerObj.forward = dirToCombatLookAt.normalized;
+
     }
 }
